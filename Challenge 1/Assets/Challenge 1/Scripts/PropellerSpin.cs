9951e14bd320472e06714cfc06dbad4fc78f8de0
Chris O <chrisdotorellana@gmail.com>
2026-03-10 21:59:59 -0400

    Day 3 - Troubleshooting
    - Fix a plane while its flying

---
+++ Challenge 1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float rotateSpeed = -2000;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+}
