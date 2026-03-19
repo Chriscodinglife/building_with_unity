f40743bab8f1b62b17d16f6fde00c8bede2725cd
Chris O <chrisdotorellana@gmail.com>
2026-03-18 21:54:01 -0400

    Day 8
    Adding Camera Movement Powerups and Enemies

---
+++ Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnManager;
+    private float boundary = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = spawnManager.enemySpeed;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+        if (transform.position.x < -boundary || transform.position.x > boundary)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < -boundary || transform.position.z > boundary)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -19,3 +19,9 @@ This is a personal project for myself to follow along the Unity Create With Code
 <p align="center" width="100%">
 <video src="https://github.com/user-attachments/assets/07db8f47-6c94-469b-af1d-86bb110d86c6" width="80%" controls></video>
 </p>
+
+### Gameplay 3
+
+<p align="center" width="100%">
+<video src="https://github.com/user-attachments/assets/ff3f20bd-6b69-4418-a41c-0459d577ff10" width="80%" controls></video>
+</p>
